19f2d764fae6cc085f55fafee7034af1f443a2ef
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-04-02 17:04:08 -0600

    correccion de errores con el next scene para altar

--- objetos/interactuables/altar.gd
+++ objetos/interactuables/altar.gd
@@ -1,23 +1,13 @@
-extends Area2D
-
+extends Node2D
 @export var next_scene_path: String
 
-# Nueva función conectada a la señal "area_entered"
-func _on_area_entered(area: Area2D) -> void:
-	# Comprueba si el área que entró está en la capa (layer) de tu Hurtbox.
-	# Cambia el '2' por el número de capa donde tienes configurado el Hurtbox del Jugador.
-	if area.get_collision_layer_value(2):
-		change_scene()
-
-func change_scene() -> void:
-	# Una buena práctica es verificar que la ruta no esté vacía antes de cambiar
-	if next_scene_path != "":
-		get_tree().change_scene_to_file(next_scene_path)
-	else:
-		print("Error: No se ha asignado una ruta para next_scene_path")
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Jugador:
 		change_scene()
 	pass # Replace with function body.
+
+func change_scene()->void:
+	if next_scene_path != "":
+		get_tree().change_scene_to_file(next_scene_path)
+	else:
+		print("Error: ruta next_scene_path no encontrada")
