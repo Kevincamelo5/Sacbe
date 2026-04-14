477b23ccb7aebbeb3b7abb7bc7c8eb733a879806
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-04-13 21:37:14 -0600

    funcionamiento del generador de vidas

--- interfaz/hud/contador_vidas.gd
+++ interfaz/hud/contador_vidas.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
-# Asegúrate de arrastrar tu nodo Label aquí desde la jerarquía, o usar el nombre correcto
 @onready var texto_vidas: Label = $TextoVidas 
 
 func _ready() -> void:
-	var game_manager = get_node("%GameManager")
-	game_manager.vida_actualizada.connect(_on_vida_actualizada)
+	# Verificamos que el GameManager esté disponible
+	var game_manager = get_node_or_null("%GameManager")
+	if game_manager:
+		# Conectamos la señal a nuestra función local
+		game_manager.vida_actualizada.connect(_on_vida_actualizada)
+		# Seteamos el valor inicial
+		_on_vida_actualizada(game_manager.vida)
 
-func  _on_vida_actualizada() -> void:
+# Agregamos 'nueva_vida' como argumento para que la función reconozca el dato
+func _on_vida_actualizada(nueva_vida: int) -> void:
 	texto_vidas.text = str(nueva_vida)
