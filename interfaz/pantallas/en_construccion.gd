9f21e26807c249dcd0d3cdd8a498dbe947ae7449
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-04-28 22:26:25 -0600

    correccion de direcciones al boton regresar

--- interfaz/pantallas/en_construccion.gd
+++ interfaz/pantallas/en_construccion.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _on_menu_principal_pressed() -> void:
-	get_tree().change_scene_to_file("res://mapa/Menu/menu_principal.tscn")
+	get_tree().change_scene_to_file("res://interfaz/menus/menu_principal.tscn")
 	pass # Replace with function body.
