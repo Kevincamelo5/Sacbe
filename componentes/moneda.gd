23e6ad1ef0e1056ed44fb6cb1650c4f2477ad4e3
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-04-29 22:34:43 -0600

    agregacion de animaciones de los elementos

--- componentes/moneda.gd
+++ componentes/moneda.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var game_manager = get_tree().current_scene.find_child("GameManager")
+@onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	animatedSprite.play("gira")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
@@ -10,6 +14,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	if body is Jugador or body.name == "Jugador" or body.is_in_group("jugador"):
 		$AudioStreamPlayer.play() 	   	
+		animatedSprite.hide()
+		$Area2D/CollisionShape2D.hide()
 		GameManager.incrementar_monedas()
 		await $AudioStreamPlayer.finished
 		queue_free()
