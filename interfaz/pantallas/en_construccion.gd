790a336410fef850b731bdd15a50d32a13adcfe7
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-05-05 21:58:56 -0600

    correccion de medidas

--- interfaz/pantallas/en_construccion.gd
+++ interfaz/pantallas/en_construccion.gd
@@ -3,4 +3,9 @@ extends Control
 
 func _on_menu_principal_pressed() -> void:
 	get_tree().change_scene_to_file("res://interfaz/menus/menu_principal.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://interfaz/menus/menu_principal.tscn")
+
 	pass # Replace with function body.
