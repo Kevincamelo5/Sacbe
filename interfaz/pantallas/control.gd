abecfbe32cd356547cf79de5efb4c05c201197e3
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2026-05-14 14:15:22 -0600

    agregaciones y estilizado

--- interfaz/pantallas/control.gd
+++ interfaz/pantallas/control.gd
@@ -26,8 +26,8 @@ func skip_cinematic():
 	cambiar_a_juego()
 
 func cambiar_a_juego():
-	get_tree().change_scene_to_file("res://niveles/pruebas.tscn")
+	get_tree().change_scene_to_file("res://niveles/nivel1/nivel1_parte1.tscn")
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://niveles/pruebas.tscn")
+	get_tree().change_scene_to_file("res://niveles/nivel1/nivel1_parte1.tscn")
